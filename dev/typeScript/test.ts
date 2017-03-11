02d106eaaa650a8b31b5eb23ddc580aa1e267df2
s-iijima <s-iijima@s-iijima-MB365.local>
2017-03-12 02:12:05 +0900

    ランディングページとりあえず完成

--- dev/typeScript/test.ts
+++ dev/typeScript/test.ts
@@ -5,18 +5,22 @@
 // $(function () {
 //     alert("hoggge");
 // });
-function AnimationTop() {
-    let camome :Vivus = new Vivus('x-svg-camome', {start: "autostart",file: '/public/img/camome.svg', type: 'oneByOne', duration: 1000}); 
-    camome.play();
+
+function AnimationTop() :void{
+    let camome :Vivus = new Vivus('x-svg-camome', {start: "autostart",file: '/public/img/camome.svg', type: 'oneByOne', duration: 800}); 
+}
+
+function TweenButton(delay = 0) :void{
+    TweenMax.from('#x-top-button', 1, { y: 200, autoAlpha:0, delay:delay});
+}
+
+function TweenTheme(delay = 0,callBack? :() => void) :void{
+    TweenMax.from('#x-theme-wrapper', 1, { y: -400, autoAlpha: 0, delay: delay,onComplete: callBack});
 }
 
 $(window).load(function () {
-    setTimeout(function(){
-        AnimationTop();
-    },3000);
+    // setTimeout(function(){
+    // },1000);
+    TweenTheme(0.1,() => AnimationTop());
+    TweenButton(7.5);
 });
-
-// new Vivus('x-remove-vec', {start: 'autostart', type: 'delayed', duration: 200}); 
-// new Vivus('x-follower-vec', {start: 'autostart', type: 'delayed', duration: 200}); 
-// new Vivus('x-return-vec', {start: 'autostart', type: 'delayed', duration: 200}); 
-// new Vivus('x-peke', {start: 'autostart', type: 'delayed', duration: 200}); 
--- public/js/test.js
+++ public/js/test.js
@@ -52,13 +52,17 @@
 /***/ function(module, exports) {
 
 	function AnimationTop() {
-	    let camome = new Vivus('x-svg-camome', { start: "autostart", file: '/public/img/camome.svg', type: 'oneByOne', duration: 1000 });
-	    camome.play();
+	    let camome = new Vivus('x-svg-camome', { start: "autostart", file: '/public/img/camome.svg', type: 'oneByOne', duration: 800 });
+	}
+	function TweenButton(delay = 0) {
+	    TweenMax.from('#x-top-button', 1, { y: 200, autoAlpha: 0, delay: delay });
+	}
+	function TweenTheme(delay = 0, callBack) {
+	    TweenMax.from('#x-theme-wrapper', 1, { y: -400, autoAlpha: 0, delay: delay, onComplete: callBack });
 	}
 	$(window).load(function () {
-	    setTimeout(function () {
-	        AnimationTop();
-	    }, 3000);
+	    TweenTheme(0.1, () => AnimationTop());
+	    TweenButton(7.5);
 	});
 
 
